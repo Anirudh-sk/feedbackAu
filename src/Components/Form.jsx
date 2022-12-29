4d9bc8a769816fba8d4ef5d4f88e6040a0789f21
ani <amazinganirudhhere@gmail.com>
2022-12-29 23:05:22 +0530

    form updates

---
+++ src/Components/Form.jsx
@@ -0,0 +1,126 @@
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import Radio from '@mui/material/Radio';
+import RadioGroup from '@mui/material/RadioGroup';
+import { VerifiedUserRounded } from '@mui/icons-material';
+
+function Copyright(props) {
+    return (
+        <Typography variant="body2" color="text.secondary" align="center" {...props}>
+            {'Copyright © '}
+            <Link color="inherit" href="https://www.annauniv.edu/">
+                Anna University
+            </Link>{' '}
+            {new Date().getFullYear()}
+            {'.'}
+        </Typography>
+    );
+}
+
+const theme = createTheme();
+
+export default function Form() {
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const data = new FormData(event.currentTarget);
+        console.log({
+            email: data.get('email'),
+            password: data.get('password'),
+            name: data.get('Name')
+        });
+    };
+
+    return (
+        <ThemeProvider theme={theme}>
+            <Container component="main" maxWidth="xs">
+                <CssBaseline />
+                <Box
+                    sx={{
+                        marginTop: 8,
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                    }}
+                >
+                    <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+                        <VerifiedUserRounded/>
+                    </Avatar>
+                    <Typography component="h1" variant="h5">
+                        Feedback Form
+                    </Typography>
+                    <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
+                        <Grid container spacing={2}>
+                            <Grid item xs={12} >
+                                <TextField
+                                    autoComplete="given-name"
+                                    name="Name"
+                                    required
+                                    fullWidth
+                                    id="Name"
+                                    label="Name"
+                                    autoFocus
+                                />
+                            </Grid>
+                            <Grid item xs={12} >
+                                <TextField
+                                    required
+                                    fullWidth
+                                    type="number"
+                                    id="Phone"
+                                    label="Phone"
+                                    name="Phone"
+                                    autoComplete="phone"
+                                />
+                            </Grid>
+                            {/* <Grid item xs={12}>
+                                <TextField
+                                    required
+                                    fullWidth
+                                    type="email"
+                                    id="email"
+                                    label="Email Address"
+                                    name="email"
+                                    autoComplete="email"
+                                />
+                            </Grid> */}
+                            <Grid item xs={12}>
+                                <RadioGroup
+                                    row
+                                    aria-labelledby="demo-row-radio-buttons-group-label"
+                                    name="row-radio-buttons-group"
+                                    
+                                >
+                                    <FormControlLabel id='student' value="student" control={<Radio />} label="Student" />
+                                    <FormControlLabel id='teacher' value="teacher" control={<Radio />} label="Teacher" />
+                                    <FormControlLabel id='guest' value="guest" control={<Radio />} label="Guest" />
+                                    
+                                </RadioGroup>
+                            </Grid>
+                            
+                        </Grid>
+                        <Button
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            sx={{ mt: 3, mb: 2 }}
+                        >
+                            Sign Up
+                        </Button>
+
+                    </Box>
+                </Box>
+                <Copyright sx={{ mt: 5 }} />
+            </Container>
+        </ThemeProvider>
+    );
+}
