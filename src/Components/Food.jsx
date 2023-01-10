71da5ebaee05f5451db94f1b5982b27d799bcd01
ani <amazinganirudhhere@gmail.com>
2023-01-10 22:29:13 +0530

    sending data to the backend

--- src/Components/Food.jsx
+++ src/Components/Food.jsx
@@ -55,7 +55,7 @@ export default function Food() {
   const [Ambience, setAmbience] = useState(5);
  
 
-  const handleSubmit = (event) => {
+  const handleSubmit = async (event) => {
     event.preventDefault();
     const data = {
       Name,
@@ -72,6 +72,17 @@ export default function Food() {
       
     };
     console.log(data);
+    const res= await fetch("http://localhost:5000/feedback",{
+      method:'POST',
+      headers:{"Content-Type": "application/json"},
+      body: JSON.stringify(data)
+    }).then(()=>{
+      console.log(res);
+      window.location='/thankyou'
+    }).catch(error=>{
+      console.log(error);
+      window.location='/error'
+    })
   };
 
   return (
