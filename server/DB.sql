3b4a678297ae99e9874317045483d708e4c506ca
Jivitesh <jivitesh.vit@gmail.com>
2023-01-12 13:35:06 +0530

    server update

--- server/DB.sql
+++ server/DB.sql
@@ -1,9 +1,41 @@
 CREATE DATABASE testdb;
 
-CREATE TABLE feedback(
+CREATE TABLE basicInfo(
     ID serial primary key UNIQUE,
     Building varchar(50) NOT NULL,
     Floor varchar(2) NOT NULL,
-    Toilet VARCHAR(1) NOT NULL,
-    Feedback VARCHAR(255)
+    Name varchar(25) NOT NULL,
+    Phone int,
+    Type varchar(15)
+);
+
+CREATE TABLE toiletForm(
+    ID INT,
+    Cleanliness INT,
+    Complaint VARCHAR(255),
+    Flushworking VARCHAR(5),
+    WashedRegularly VARCHAR(5),
+    WaterLeakage VARCHAR(5),
+    WaterSupply VARCHAR(5),
+    CONSTRAINT fk_ID FOREIGN KEY(ID) REFERENCES basicInfo(ID)
+);
+
+CREATE TABLE foodForm(
+    ID INT,
+    Ambience INT,
+    Cleanliness INT,
+    Feedback VARCHAR(255),
+    FoodQuality INT,
+    FoodTaste INT,
+    ServiceQuality INT,
+    CONSTRAINT fk_ID FOREIGN KEY(ID) REFERENCES basicInfo(ID) 
+);
+
+CREATE TABLE securityForm(
+    ID INT,
+    SecurityAlertness INT,
+    SecurityAvailability INT,
+    SecurityDrunk VARCHAR(3),
+    SecurityMisbehaving VARCHAR(3),
+    CONSTRAINT fk_ID FOREIGN KEY(ID) REFERENCES basicInfo(ID)
 );
--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const pool = require("./db");
+// const { prototype } = require("pg/lib/connection-parameters");
 
 app.use(cors())
 app.use(express.json())
@@ -10,8 +11,38 @@ app.use(express.json())
 app.post("/feedback",async (req,res)=>{
     try {
         console.log(req.body);
-        const newUser= await pool.query("INSERT INTO feedback (Building, Floor, Toilet,Feedback) VALUES($1, $2, $3, $4) RETURNING *",[req.body.building,req.body.floor, req.body.toilet, req.body.feedback]);
-        res.json(newUser)
+        // const newUser = await pool.query("select * from pg_catalog.pg_tables;");
+        console.log('inserting');
+        const newUser= await pool.query("INSERT INTO basicInfo (Building, Floor, Name, Phone, Type) VALUES($1, $2, $3, $4, $5) RETURNING *",[req.body.Building,req.body.floor,req.body.Name,req.body.Phone,req.body.Type]);
+        if(typeof req.body.flushworking != "undefined"){
+            console.log("Entered flush");
+            const flush = await pool.query("INSERT INTO toiletForm (ID, Cleanliness, \
+                Complaint, Flushworking, WashedRegularly, WaterLeakage, WaterSupply) VALUES($1, $2, $3, $4, $5, $6, $7)",[newUser['rows'][0]['id'], req.body.Cleanliness, req.body.Complaint, req.body.FlushWorking, req.body.WashedRegularly, req.body.WaterLekage, req.body.WaterSupply]);
+                // res.json({newUser, flush});
+                // console.log(newUser);
+                // console.log(newUser['rows'][0]['id'])
+        }
+
+        else if(typeof req.body.SecurityAvailability != "undefined"){
+            console.log("Entered Security");
+            const security = await pool.query("INSERT INTO securityForm (ID, Feedback, \
+                SecurityAvailability, SecurityMisbehaving, SecurityDrunk, SecurityAlertness) VALUES($1, $2, $3, $4, $5, $6)",[newUser['rows'][0]['id'], req.body.Feedback, req.body.SecurityAvailability, req.body.SecurityMisbehaving, req.body.SecurityDrunk, req.body.SecurityAlertness]);
+                // res.json({newUser, flush});
+                // console.log(newUser);
+                // console.log(newUser['rows'][0]['id'])
+        }
+
+        else if(typeof req.body.FoodQuality != "undefined"){
+            console.log("Entered Security");
+            const food = await pool.query("INSERT INTO securityForm (ID, Feedback, \
+                Cleanliness, FoodQuality, FoodTaste, ServiceQuality, Ambience) VALUES($1, $2, $3, $4, $5, $6, $7)",[newUser['rows'][0]['id'], req.body.Feedback, req.body.Cleanliness, req.body.FoodQuality, req.body.FoodTaste, req.body.ServiceQuality, req.body.Ambience]);
+                // res.json({newUser, flush});
+                // console.log(newUser);
+                // console.log(newUser['rows'][0]['id'])
+        }
+        
+
+        // res.json({newUser});
         // res.json(newDept)
         
     } catch (error) {
@@ -20,15 +51,15 @@ app.post("/feedback",async (req,res)=>{
 })
 
 //get all users
-app.get("/feedback",async(req,res)=>{
-    try {
-        const allUsers = await pool.query("SELECT * FROM feedback");
-        res.json(allUsers.rows)
-        // res.send(allUsers);
-    } catch (error) {
-        console.error(error);
-    }
-})
+// app.get("/feedback",async(req,res)=>{
+//     try {
+//         const allUsers = await pool.query("SELECT * FROM feedback");
+//         res.json(allUsers.rows)
+//         // res.send(allUsers);
+//     } catch (error) {
+//         console.error(error);
+//     }
+// })
 
 //get all departments
 // app.get("/department",async(req,res)=>{
@@ -42,38 +73,38 @@ app.get("/feedback",async(req,res)=>{
 // })
 
 //get specific users
-app.get("/feedback/:id",async(req,res)=>{
-    try {
-        const {id}=req.params;
-        const SpecificUser = await pool.query("SELECT * FROM feedback WHERE ID = $1",[id]);
-        res.json(SpecificUser.rows)
-        // res.send(allUsers);
-    } catch (error) {
-        console.error(error);
-    }
-})
+// app.get("/feedback/:id",async(req,res)=>{
+//     try {
+//         const {id}=req.params;
+//         const SpecificUser = await pool.query("SELECT * FROM feedback WHERE ID = $1",[id]);
+//         res.json(SpecificUser.rows)
+//         // res.send(allUsers);
+//     } catch (error) {
+//         console.error(error);
+//     }
+// })
 
 //update a user department
-app.put("/feedback/:id",async(req,res)=>{
-    try {
-        const {id}=req.params;
-        const {Department}=req.body;
-        const update= await pool.query("UPDATE feedback SET Feedback = $1 WHERE ID = $2",[Department,id])
-        res.json("Feedback Updated Successfully")
+// app.put("/feedback/:id",async(req,res)=>{
+//     try {
+//         const {id}=req.params;
+//         const {Department}=req.body;
+//         const update= await pool.query("UPDATE feedback SET Feedback = $1 WHERE ID = $2",[Department,id])
+//         res.json("Feedback Updated Successfully")
 
         
-    } catch (error) {
-        console.error(error);
-    }
-})
+//     } catch (error) {
+//         console.error(error);
+//     }
+// })
 
 
 //delete a user 
-app.delete("/feedback/:id",async(req,res)=>{
-    const {id}=req.params;
-    const deleteUser= await pool.query("DELETE FROM feedback where ID = $1",[id]);
-    res.json("Feedback Deleted")
-})
+// app.delete("/feedback/:id",async(req,res)=>{
+//     const {id}=req.params;
+//     const deleteUser= await pool.query("DELETE FROM feedback where ID = $1",[id]);
+//     res.json("Feedback Deleted")
+// })
 
 app.listen(5000, ()=>{
     console.log("server on http://localhost:5000");
